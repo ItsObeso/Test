3e395a47757ebeee5fbe9face2a3f4bd9a2ffd55
ItsObeso <65413750+ItsObeso@users.noreply.github.com>
2020-05-15 09:46:56 -0700

    Add files via upload

---
+++ MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void EndTheGame()
+    {
+        print("bye bye");
+        Application.Quit();
+    }
+
+    public void ProcedetoGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void GoToCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
